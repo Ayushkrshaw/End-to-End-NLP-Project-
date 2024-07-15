9daf4cbdeecaaa2086d60930aa3e21c8adca0135
Ayush <ayush2106kumarshaw@gmail.com>
2024-07-16 00:24:17 +0530

    Changes done

--- circleci_setup_template.sh
+++ circleci_setup_template.sh
@@ -43,7 +43,7 @@ sudo nano /etc/opt/circleci/launch-agent-config.yaml
 
 # Add API in the file and change permissions
 api:
-  auth_token: 17f70d59d059a9c24d6e37d51afb969f8ccf10124be54033a0a2b0f8a645a7e187ec1551d3d07042
+  auth_token: 2d2d4b0eccfa5fa6fd8fd18ef299169ddeb1e81395c22abef03e13e758ab1a8a2f7b1b53cb286b59
 
 runner:
   name: self-hosted
